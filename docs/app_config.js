d0fa6220b73c19f356e9fa14fe0ea9fa16b3241a
artskin <artskin@163.com>
2021-03-14 23:25:06 +0800

    add pages

---
+++ docs/app_config.js
@@ -0,0 +1,17 @@
+
+//apiserver 和前端在同一个服务器时host=''
+let host =""
+
+window.appConf = {
+  "lang":"en",
+  "theme":"",
+  "mWidth":900,
+  "api":{
+    login    : `${host}/users/login`,
+    logout   : `${host}/users/logout`,
+    userInfo : `${host}/users/info`,
+    sysInfo  : `${host}/sys/info`,
+    tasks    : `${host}/task/list`,
+    statistics:`${host}/statistics`
+  }
+}
